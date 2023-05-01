e76f64ad020d561b2df817b31c6ec1c22451baad
pm017 <priyabrata007.m@gmail.com>
2023-05-01 16:23:36 +0000

    option part completed

--- project/lstm_flow.py
+++ project/lstm_flow.py
@@ -1,10 +1,23 @@
 from metaflow import FlowSpec, step, Flow, current, Parameter, IncludeFile, card, current
 from metaflow.cards import Table, Markdown, Artifact
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Model
+from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
+from keras.optimizers import RMSprop
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing import sequence
+from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
 
 # TODO move your labeling function from earlier in the notebook here
 labeling_function = lambda row: 0 if row['rating'] < 4 else 1
 
-class BaselineNLPFlow(FlowSpec):
+class GoodFirstModelNLPFlow(FlowSpec):
 
     # We can define input parameters to a Flow using Parameters
     # More info can be found here https://docs.metaflow.org/metaflow/basics#how-to-define-parameters-for-flows
@@ -48,23 +61,56 @@ class BaselineNLPFlow(FlowSpec):
 
         self.next(self.baseline)
 
+    # def RNN():
+    #     inputs = Input(name='inputs',shape=[max_len])
+    #     layer = Embedding(max_words,50,input_length=max_len)(inputs)
+    #     layer = LSTM(64)(layer)
+    #     layer = Dense(256,name='FC1')(layer)
+    #     layer = Activation('relu')(layer)
+    #     layer = Dropout(0.5)(layer)
+    #     layer = Dense(1,name='out_layer')(layer)
+    #     layer = Activation('sigmoid')(layer)
+    #     model = Model(inputs=inputs,outputs=layer)
+    # return model
+
     @step
     def baseline(self):
         "Compute the baseline"
         
         ### TODO: Fit and score a baseline model on the data, log the acc and rocauc as artifacts.
+        max_words = 1000
+        max_len = 150
+        tok = Tokenizer(num_words=max_words)
+        tok.fit_on_texts(self.traindf['review'])
+        sequences = tok.texts_to_sequences(self.traindf['review'])
+        sequences_matrix = sequence.pad_sequences(sequences,maxlen=max_len)
+
         from sklearn.metrics import accuracy_score, roc_auc_score
-        from sklearn.dummy import DummyClassifier
-        dummy_clf = DummyClassifier(strategy="most_frequent")
-        dummy_clf.fit(self.traindf['review'], self.traindf['label'])
-        DummyClassifier(strategy='most_frequent')
-        self.valdf['preds']=dummy_clf.predict(self.valdf['review'])
+        inputs = Input(name='inputs',shape=[max_len])
+        layer = Embedding(max_words,50,input_length=max_len)(inputs)
+        layer = LSTM(64)(layer)
+        layer = Dense(256,name='FC1')(layer)
+        layer = Activation('relu')(layer)
+        layer = Dropout(0.5)(layer)
+        layer = Dense(1,name='out_layer')(layer)
+        layer = Activation('sigmoid')(layer)
+        model = Model(inputs=inputs,outputs=layer)
+        model.summary()
+        model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
+
+        model.fit(sequences_matrix,self.traindf['label'],batch_size=128,epochs=10,
+          validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
+        test_sequences = tok.texts_to_sequences(self.valdf['review'])
+        test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len)
+        preds = model.predict(test_sequences_matrix ).round()
+        self.valdf['preds'] = preds
         # self.vald_pred = [1]*self.valdf.shape[0]
         # self.valdf['preds'] = self.vald_pred
         self.base_acc = accuracy_score(self.valdf['label'],self.valdf['preds'] )
         self.base_rocauc = roc_auc_score(self.valdf['label'],self.valdf['preds'] )
         
         self.next(self.end)
+
         
     @card(type='corise') # TODO: after you get the flow working, chain link on the left side nav to open your card!
     @step
@@ -97,6 +143,6 @@ class BaselineNLPFlow(FlowSpec):
             Table.from_dataframe(self.fn_df)
         )
 if __name__ == '__main__':
-    BaselineNLPFlow()
+    GoodFirstModelNLPFlow()
 
 
